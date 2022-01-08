702bb60978e17e77ca25794fa157d289f26da7e3
Daniel Klinkert <daniel.klinkert@gmail.com>
2022-01-08 10:52:15 +0100

    use tabs

--- PintAR/Presentation/CameraViewModel.swift
+++ PintAR/Presentation/CameraViewModel.swift
@@ -11,14 +11,14 @@ import Combine
 
 class CameraViewModel {
 
-    @Published var recognizedText: [String] = []
-    @Published var objectFrame: CGRect = .zero
-    @Published var objectIdentifier: String = ""
-    @Published var shapes: [CGPath] = []
-    @Published var rgb: ColorDetection.RGBA = (0, 0, 0, 0)
+	@Published var recognizedText: [String] = []
+	@Published var objectFrame: CGRect = .zero
+	@Published var objectIdentifier: String = ""
+	@Published var shapes: [CGPath] = []
+	@Published var rgb: ColorDetection.RGBA = (0, 0, 0, 0)
 
-    private var cancellableSet: Set<AnyCancellable> = []
-    private let detectObjectUseCase: DetectObjectUseCaseProtocol
+	private var cancellableSet: Set<AnyCancellable> = []
+	private let detectObjectUseCase: DetectObjectUseCaseProtocol
 
 	init(detectObjectUseCase: DetectObjectUseCaseProtocol) {
 		self.detectObjectUseCase = detectObjectUseCase
@@ -27,41 +27,41 @@ class CameraViewModel {
 	func configureVision() {
 		self.detectObjectUseCase.setupObjectDetection()
 
-        self.setupSubscribers()
+		self.setupSubscribers()
 	}
 
-    private func setupSubscribers() {
-        guard let results = (detectObjectUseCase as? DetectObjectUseCase)?.results else {
-            return
-        }
+	private func setupSubscribers() {
+		guard let results = (detectObjectUseCase as? DetectObjectUseCase)?.results else {
+			return
+		}
 
-        for key in results.keys {
-            let value = results[key]
-            switch key {
-            case .text(fastRecognition: _):
-                TextRecognition.convert(value: value)?
-                    .assign(to: \.recognizedText, on: self)
-                    .store(in: &cancellableSet)
-            case .rectangles(model: _):
-                RectangleDetection.convert(value: value)?
-                    .sink(receiveValue: { (identifier, frame) in
-                        self.objectIdentifier = identifier
-                        self.objectFrame = frame
-                    })
-                    .store(in: &cancellableSet)
-            case .contour:
-                ContourDetection.convert(value: value)?
-                    .assign(to: \.shapes, on: self)
-                    .store(in: &cancellableSet)
-            case .color:
-                ColorDetection.convert(value: value)?
-                    .assign(to: \.rgb, on: self)
-                    .store(in: &cancellableSet)
-            }
-        }
-    }
+		for key in results.keys {
+			let value = results[key]
+			switch key {
+				case .text(fastRecognition: _):
+					TextRecognition.convert(value: value)?
+						.assign(to: \.recognizedText, on: self)
+						.store(in: &cancellableSet)
+				case .rectangles(model: _):
+					RectangleDetection.convert(value: value)?
+						.sink(receiveValue: { (identifier, frame) in
+							self.objectIdentifier = identifier
+							self.objectFrame = frame
+						})
+						.store(in: &cancellableSet)
+				case .contour:
+					ContourDetection.convert(value: value)?
+						.assign(to: \.shapes, on: self)
+						.store(in: &cancellableSet)
+				case .color:
+					ColorDetection.convert(value: value)?
+						.assign(to: \.rgb, on: self)
+						.store(in: &cancellableSet)
+			}
+		}
+	}
 
 	func recognizeObject(image: CVImageBuffer) {
-        self.detectObjectUseCase.recognizeObject(in: image)
+		self.detectObjectUseCase.recognizeObject(in: image)
 	}
 }

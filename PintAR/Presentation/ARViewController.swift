60fd53526e3709f2eb658b5fe7640e28e4ca1560
Daniel Klinkert <daniel.klinkert@gmail.com>
2022-01-27 20:05:00 +0100

    extract functions for scnode

--- PintAR/Presentation/ARViewController.swift
+++ PintAR/Presentation/ARViewController.swift
@@ -175,19 +175,3 @@ class ARViewController: UIViewController, UIGestureRecognizerDelegate, ARSession
 		return textNode
 	}
 }
-
-extension SCNNode {
-	var height: Float {
-		return (boundingBox.max.y - boundingBox.min.y) * scale.y
-	}
-
-	func pivotOnTopLeft() {
-		let (min, max) = boundingBox
-		pivot = SCNMatrix4MakeTranslation(min.x, max.y, 0)
-	}
-
-	func pivotOnTopCenter() {
-		let (_, max) = boundingBox
-		pivot = SCNMatrix4MakeTranslation(0, max.y, 0)
-	}
-}
